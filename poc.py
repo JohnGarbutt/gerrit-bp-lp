cf4b93afe19b34ea9d652c426a33b60ceeb69fe8
Joe Gordon <joe.gordon0@gmail.com>
2014-02-05 17:51:08 -0800

    Distinguish between unapproved and unknown blueprints

--- poc.py
+++ poc.py
@@ -19,6 +19,17 @@ def get_approved_bluerpint(project):
         raise Exception("Not a OpenStack project!")
     return [bp.name for bp in proj.valid_specifications]
 
+def get_invalid_blueprints(project, valid):
+    lp = launchpad.Launchpad.login_anonymously('grabbing BPs',
+                                               'production',
+                                               LPCACHEDIR,
+                                               version='devel')
+    proj = lp.projects(project)
+    if "openstack" not in proj.project_group.name:
+        raise Exception("Not a OpenStack project!")
+    all_bps = [bp.name for bp in proj.all_specifications]
+    return [x for x in all_bps if x not in valid]
+
 
 def get_blueprint(message):
     """If no blueprint, return None."""
@@ -29,7 +40,7 @@ def get_blueprint(message):
         return m.group(1)
 
 
-def get_unapproved_blueprint_patches(approved_blueprints):
+def get_unapproved_blueprint_patches(approved_blueprints, invalid_blueprints):
     """Return a list of patches with unapproved blueprints."""
     result = {} #URL: BP
     gerrit = gerritlib.gerrit.Gerrit("review.openstack.org", "jogo", 29418)
@@ -40,13 +51,17 @@ def get_unapproved_blueprint_patches(approved_blueprints):
         bp = get_blueprint(msg)
         if bp is not None:
             if bp not in approved_blueprints:
-                result[patch['url']]=bp
+                if bp in invalid_blueprints:
+                    result[patch['url']]=bp
+                else:
+                    result[patch['url']]=("%s (unknown)" % bp)
     return result
 
 
 def main():
     approved_blueprints = get_approved_bluerpint("nova")
-    patches =  get_unapproved_blueprint_patches(approved_blueprints)
+    invalid_blueprints = get_invalid_blueprints("nova", approved_blueprints)
+    patches =  get_unapproved_blueprint_patches(approved_blueprints, invalid_blueprints)
     print "patches with unapproved blueprints"
     for k in patches:
         print "%s: %s" % (k, patches[k])
